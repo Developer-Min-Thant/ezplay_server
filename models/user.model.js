ab969f3c00b0dfb182b336f4af28253fa3ca8e4e
BlackPantherLion <BlackPantherLion@gmail.com>
2025-04-29 17:13:19 +0630

    Remove date field from user schema, keeping deviceId and count fields

--- models/user.model.js
+++ models/user.model.js
@@ -43,11 +43,6 @@ const userSchema = new mongoose.Schema({
   deviceId: {
     type: String,
   },
-  date: {
-    type: String, // Format: 'YYYY-MM-DD'
-    required: true,
-    index: true,
-  },
   count: {
     type: Number,
     required: true,
