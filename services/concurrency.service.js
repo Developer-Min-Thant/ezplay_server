57c8275b5156fa8311985e7b0808c9a6e9e0223f
BlackPantherLion <BlackPantherLion@gmail.com>
2025-06-01 19:46:29 +0630

    config: increase max concurrent downloads from 10 to 50

--- services/concurrency.service.js
+++ services/concurrency.service.js
@@ -4,7 +4,7 @@
 
 // Track active downloads
 let activeDownloads = 0;
-const MAX_CONCURRENT_DOWNLOADS = 10;
+const MAX_CONCURRENT_DOWNLOADS = 50;
 
 // Increment active downloads count
 const incrementActiveDownloads = () => {
