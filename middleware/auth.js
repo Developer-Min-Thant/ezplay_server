03bdb55fde1d36682884b5e54dadf1eba2737b69
BlackPantherLion <BlackPantherLion@gmail.com>
2025-06-15 14:22:47 +0630

    feat: add device ID mismatch logging in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -66,6 +66,7 @@ exports.checkDownloadEligibility = async (req, res, next) => {
       });
     }
     if(user.deviceId !== req.body.deviceId){
+      console.log('Device ID mismatch', user.deviceId, req.body.deviceId);
       return res.status(401).json({
         success: false,
         message: 'Device ID mismatch'
